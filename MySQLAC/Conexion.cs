efea433133de53187cafba371a7dbb4927e16ed3
M-Aranda <cheloz_20@hotmail.com>
2018-12-30 21:09:52 -0300

    primer commit para probar conexion a MySQL desde NET

---
+++ MySQLAC/Conexion.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient; //esta es una referencia al conector de MySQL para NET que debe descargarse con anterioridad
+
+namespace MySQLAC
+{
+    class Conexion
+    {
+        private MySqlConnection connection; //la conexion en si
+        private string server; //el servidor, usualmente localhost
+        private string database; //el nombre de la base de datos
+        private string uid; //el nombre del usuario accediendo a la bd
+        private string password; // la constrasenia del usuario accediendo a la bd
+
+
+        public Conexion()
+        {
+            Initialize();
+        }
+
+
+        private void Initialize()
+        {
+            server = "localhost";
+            database = "baseDePrueba";
+            uid = "root";
+            password = "";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        //open connection to database
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+
+
+        public void Insert()
+        {
+            string query = "INSERT INTO persona  VALUES(NULL, 'Marcelo')";
+
+            //open connection
+            if (this.OpenConnection() == true)
+            {
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //Execute command
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+        }
+
+
+
+
+    }
+}
---
+++ scriptPruebaNETMYSQL.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE baseDePrueba;
+
+USE baseDePrueba;
+
+CREATE TABLE persona (
+id INT AUTO_INCREMENT,
+nombre VARCHAR (30),
+PRIMARY KEY (id)
+);
+
+/*
+SELECT * FROM persona;
+DROP DATABASE baseDePrueba;
+
+*/
